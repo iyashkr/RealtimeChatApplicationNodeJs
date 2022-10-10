00247c52a550b60efc9f9847119b840b8da3a89d
iyashkr <kumaryash1212@gmail.com>
2022-10-10 21:35:54 +0530

    message event added

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,14 +1,19 @@
 // Node Server which will handle Socket.io connections
 
-const io = require("socket.io")(8000) // 8000 is the port name
+const io = require("socket.io")(8000); // 8000 is the port name
 
-const users = {
+const users = {};
 
-};
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
 
-io.on('connection', socket => {
-    socket.on('new-user-joined', name => {
-        users[socket.id] = name;
-        socket.broadcast.emmit('user-joined', name)
-    })
-})
+  socket.on("send", (message) => {
+    socket.broadcast.emit("recieve", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+});
